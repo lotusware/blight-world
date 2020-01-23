c77e0a8bffaced6c33bfafc90a7cd9565ef079fb
lotusware <lotusware@gmail.com>
2020-01-22 21:49:43 -0400

    Add files via upload

--- js/load.json.refresh.js
+++ js/load.json.refresh.js
@@ -1,6 +1,6 @@
 
   var refresher = setInterval(timer, 1000);
-  function timer() {$.getJSON("https://spreadsheets.google.com/feeds/list/1JPCdDNxz002gK2zQC5ailXhYwDpTXv1z7Bg8kdwckJE/10/public/full?alt=json", function(data) {
+  function timer() {$.getJSON("https://spreadsheets.google.com/feeds/list/1JPCdDNxz002gK2zQC5ailXhYwDpTXv1z7Bg8kdwckJE/1/public/full?alt=json", function(data) {
      var sheetData = data.feed.entry;
 
 var CURAP = data.feed.entry[0][playerName]['$t'];

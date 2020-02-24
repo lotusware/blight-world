25cf193df2faa219fe6aaae027cc7bae92ec683a
lotusware <lotusware@gmail.com>
2020-02-23 21:02:48 -0400

    changed timers

--- js/load-json-assets.js
+++ js/load-json-assets.js
@@ -1,6 +1,6 @@
 
 
-  var refresher = setInterval(timer, 2000);
+  var refresher = setInterval(timer, 10000);
   function timer() {$.getJSON("https://spreadsheets.google.com/feeds/list/1JPCdDNxz002gK2zQC5ailXhYwDpTXv1z7Bg8kdwckJE/1/public/full?alt=json", function(data) {
 
 // Assign JSON to variable	// Assign variable to element
--- js/load-json-refresh.js
+++ js/load-json-refresh.js
@@ -1,5 +1,5 @@
 
-  var refresher = setInterval(timer, 5000);
+  var refresher = setInterval(timer, 3000);
   function timer() {$.getJSON("https://spreadsheets.google.com/feeds/list/1JPCdDNxz002gK2zQC5ailXhYwDpTXv1z7Bg8kdwckJE/1/public/full?alt=json", function(data) {
 
 // Assign JSON to variable	// Assign variable to element
